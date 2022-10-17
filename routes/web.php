45ed19f64d72c863fa89090bf1354c6f6dc9a6b4
aliflhh25 <alifluqmanh12@gmail.com>
2022-10-17 20:06:36 +0700

    menyelesaikan video 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Alif Luqman",
+        "email" => "alifluqman994@gmail.com",
+        "image" => "alifl.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
